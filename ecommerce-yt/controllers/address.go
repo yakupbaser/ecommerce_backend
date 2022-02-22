8d20c5839a8583635c57e0f2af03479f3f902341
yakupbaser <yakupbaser@hotmail.com.tr>
2022-02-22 18:10:33 +0300

    init

---
+++ ecommerce-yt/controllers/address.go
@@ -0,0 +1,19 @@
+package controllers
+
+import()
+
+func AddAddress() gin.HandlerFunc{
+
+}
+
+func EditAddress() gin.HandlerFunc{
+
+}
+
+func EditWorkAddress() gin.HandlerFunc{
+
+}
+
+func DeleteAddress() gin.HandlerFunc{
+	
+}
